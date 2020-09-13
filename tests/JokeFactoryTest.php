84b59b1bf0dc866f6bd2be5f05bddca0615f5c7a
Omar Jasseh <jassehomar@users.noreply.github.com>
2020-09-13 01:07:55 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -2,8 +2,8 @@
 
 namespace JassehOmar\ChuckNorrisJokes\Tests;
 
-use PHPUnit\Framework\TestCase;
 use JassehOmar\ChuckNorrisJokes\JokeFactory;
+use PHPUnit\Framework\TestCase;
 
 class JokeFactoryTest extends TestCase
 {
@@ -19,28 +19,26 @@ class JokeFactoryTest extends TestCase
         $this->assertSame('This is a joke', $joke);
     }
 
-
     /** @test  */
     public function it_returns_a_predefined_joke()
     {
         $chuckNorrisJokes =
             [
-                "Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.",
-                "Time waits for no man. Unless that man is Chuck Norris.",
-                "If Chuck Norris were to travel to an alternate dimension in which there was another Chuck Norris and they both fought, they would both win.",
-                "Chuck Norris’ tears cure cancer. Too bad he has never cried.",
-                "Since 1940, the year Chuck Norris was born, roundhouse kick related deaths have increased 13,000 percent.",
-                "Chuck Norris does not use spell check. If he happens to misspell a word, Oxford will change the spelling.",
-                "Chuck Norris’ calendar goes straight from March 31st to April 2nd, because no one fools Chuck Norris.",
-                "The Great Wall of China was originally created to keep Chuck Norris out. It didn’t work.",
-                "Chuck Norris can divide by zero.",
-                "Chuck Norris invented airplanes because he was tired of being the only person that could fly."
+                'Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.',
+                'Time waits for no man. Unless that man is Chuck Norris.',
+                'If Chuck Norris were to travel to an alternate dimension in which there was another Chuck Norris and they both fought, they would both win.',
+                'Chuck Norris’ tears cure cancer. Too bad he has never cried.',
+                'Since 1940, the year Chuck Norris was born, roundhouse kick related deaths have increased 13,000 percent.',
+                'Chuck Norris does not use spell check. If he happens to misspell a word, Oxford will change the spelling.',
+                'Chuck Norris’ calendar goes straight from March 31st to April 2nd, because no one fools Chuck Norris.',
+                'The Great Wall of China was originally created to keep Chuck Norris out. It didn’t work.',
+                'Chuck Norris can divide by zero.',
+                'Chuck Norris invented airplanes because he was tired of being the only person that could fly.',
             ];
 
         $jokes = new JokeFactory();
 
         $joke = $jokes->getRandomJoke();
-
 
         $this->assertContains($joke, $chuckNorrisJokes);
     }
