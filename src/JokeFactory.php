73d3736ceb2969cb672cf7f56f4bcddb4861e543
Omar Jasseh <jassehomar99@gmail.com>
2020-09-13 01:08:02 +0000

    Apply fixes from StyleCI (#1)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,22 +2,20 @@
 
 namespace JassehOmar\ChuckNorrisJokes;
 
-
 class JokeFactory
 {
     protected $jokes = [
-        "Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.",
-        "Time waits for no man. Unless that man is Chuck Norris.",
-        "If Chuck Norris were to travel to an alternate dimension in which there was another Chuck Norris and they both fought, they would both win.",
-        "Chuck Norris’ tears cure cancer. Too bad he has never cried.",
-        "Since 1940, the year Chuck Norris was born, roundhouse kick related deaths have increased 13,000 percent.",
-        "Chuck Norris does not use spell check. If he happens to misspell a word, Oxford will change the spelling.",
-        "Chuck Norris’ calendar goes straight from March 31st to April 2nd, because no one fools Chuck Norris.",
-        "The Great Wall of China was originally created to keep Chuck Norris out. It didn’t work.",
-        "Chuck Norris can divide by zero.",
-        "Chuck Norris invented airplanes because he was tired of being the only person that could fly."
+        'Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.',
+        'Time waits for no man. Unless that man is Chuck Norris.',
+        'If Chuck Norris were to travel to an alternate dimension in which there was another Chuck Norris and they both fought, they would both win.',
+        'Chuck Norris’ tears cure cancer. Too bad he has never cried.',
+        'Since 1940, the year Chuck Norris was born, roundhouse kick related deaths have increased 13,000 percent.',
+        'Chuck Norris does not use spell check. If he happens to misspell a word, Oxford will change the spelling.',
+        'Chuck Norris’ calendar goes straight from March 31st to April 2nd, because no one fools Chuck Norris.',
+        'The Great Wall of China was originally created to keep Chuck Norris out. It didn’t work.',
+        'Chuck Norris can divide by zero.',
+        'Chuck Norris invented airplanes because he was tired of being the only person that could fly.',
     ];
-
 
     public function __construct(array $jokes = null)
     {
@@ -25,7 +23,6 @@ class JokeFactory
             $this->jokes = $jokes;
         }
     }
-
 
     public function getRandomJoke()
     {
